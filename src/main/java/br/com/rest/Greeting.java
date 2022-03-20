4af08b9efaefaad3386a9c3f07da0d6d0a200e2e
gabrisc <gabrisc>
2022-03-20 17:05:35 -0300

    Teste de endpoint

---
+++ src/main/java/br/com/rest/Greeting.java
@@ -0,0 +1,15 @@
+package br.com.rest;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+public class Greeting {
+
+    private final Long id;
+    private final String content;
+
+
+}
